361994d77e9008965bef28beef2f44e34bd6a5b5
AlmondChips <chips6t@gmail.com>
2023-08-20 10:26:15 +0300

    feat: add swagger /doc page with api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
+import * as yaml from 'js-yaml';
 
 import 'reflect-metadata';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 config();
 
@@ -12,6 +16,13 @@ const port = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const apiDocYaml = yaml.load(
+    await readFile(join(__dirname, '../doc/api.yaml'), { encoding: 'utf-8' }),
+  );
+
+  SwaggerModule.setup('doc', app, apiDocYaml as OpenAPIObject);
+
   await app.listen(port, () =>
     console.log(`Server is listening on port!!! `, port),
   );
